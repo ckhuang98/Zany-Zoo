5eecabffdad5d283941cc553af42e8bef965b08f
Philip Cook <pecook@ucsc.edu>
2020-05-21 02:02:47 -0700

    City Loop Ready

--- src/scenes/City.js
+++ src/scenes/City.js
@@ -24,13 +24,20 @@ class City extends Phaser.Scene {
         this.gym = this.add.image(720, 470, 'gym');
         this.apartment = this.add.image(185, 270, 'apartment');
         this.energy = 3;
+
+        this.energyMenu = this.add.text(243, 740, `${this.energy}`, { fontFamily: 'Times New Roman', fontSize: '40px', color: '#FFFFFF'}).setOrigin(0.5);
+        this.endMenu = this.add.text(592, 800, `${END}`, { fontFamily: 'Times New Roman', fontSize: '40px', color: '#FFFFFF'}).setOrigin(0.5);
+        this.strMenu = this.add.text(293, 856, `${STR}`, { fontFamily: 'Times New Roman', fontSize: '40px', color: '#FFFFFF'}).setOrigin(0.5);
+        this.witMenu = this.add.text(491, 857, `${WIT}`, { fontFamily: 'Times New Roman', fontSize: '40px', color: '#FFFFFF'}).setOrigin(0.5);
+        this.dexMenu = this.add.text(785, 857, `${DEX}`, { fontFamily: 'Times New Roman', fontSize: '40px', color: '#FFFFFF'}).setOrigin(0.5);
+        this.dayMenu = this.add.text(267, 800, `${15 - DAY}`, { fontFamily: 'Times New Roman', fontSize: '40px', color: '#FFFFFF'}).setOrigin(0.5);
+        this.hpMenu = this.add.text(837, 798, `${2 * (END - 1) + 14}`, { fontFamily: 'Times New Roman', fontSize: '40px', color: '#FFFFFF'}).setOrigin(0.5);
+
         let inEvent = false;
 
 
         //library event if clicked
         this.library.setInteractive().on('pointerdown',(pointer, localX, localY, event)=>{
-            console.log('clicked');
-            console.log(inEvent);
             if(!inEvent){
                 inEvent = true;
                 //this.scene.start("battleScene");
@@ -39,23 +46,28 @@ class City extends Phaser.Scene {
                 this.minus = this.add.image(325, 430, 'minus').setOrigin(0, 0);
                 this.exit = this.add.image(450, 750, 'exit');
                 this.energyDisplay = this.add.text(450, 645, `${this.energy}`, { fontFamily: 'Times New Roman', fontSize: '60px', color: '#FFFFFF'}).setOrigin(0.5);
+                this.skillDisplay = this.add.text(450, 465, `${WIT}`, { fontFamily: 'Times New Roman', fontSize: '60px', color: '#FFFFFF'}).setOrigin(0.5);
 
                 this.plus.setInteractive().on('pointerdown',(pointer, localX, localY, event)=>{
                     if(this.energy > 0){
-                    this.energy--;
-                    this.energyDisplay.setText(`${this.energy}`);
+                        WIT++;
+                        this.energy--;
+                        this.skillDisplay.setText(`${WIT}`);
+                        this.energyDisplay.setText(`${this.energy}`);
                     }
                 });
                 this.minus.setInteractive().on('pointerdown',(pointer, localX, localY, event)=>{
                     if(this.energy < 3){
+                        WIT--;
                         this.energy++;
+                        this.skillDisplay.setText(`${WIT}`);
                         this.energyDisplay.setText(`${this.energy}`);
                         }
                 });
                 this.exit.setInteractive().on('pointerdown',(pointer, localX, localY, event)=>{
                     if(this.energy == 0){
-                        if(Day % 2 == 1){
-                            Day++;
+                        if(DAY % 2 == 1){
+                            DAY++;
                             this.scene.start("cityScene");
                         }else{
                             this.scene.start("battleScene");
@@ -71,8 +83,6 @@ class City extends Phaser.Scene {
 
         //apartment event if clicked
         this.apartment.setInteractive().on('pointerdown',(pointer, localX, localY, event)=>{
-            console.log('clicked');
-            console.log(inEvent);
             if(!inEvent){
                 inEvent = true;
                 this.menu = this.add.image(450, 450, 'apartmentMenu');
@@ -80,22 +90,27 @@ class City extends Phaser.Scene {
                 this.minus = this.add.image(325, 430, 'minus').setOrigin(0, 0);
                 this.exit = this.add.image(450, 750, 'exit');
                 this.energyDisplay = this.add.text(450, 640, `${this.energy}`, { fontFamily: 'Times New Roman', fontSize: '60px', color: '#FFFFFF'}).setOrigin(0.5);
+                this.skillDisplay = this.add.text(450, 465, `${DEX}`, { fontFamily: 'Times New Roman', fontSize: '60px', color: '#FFFFFF'}).setOrigin(0.5);
                 this.plus.setInteractive().on('pointerdown',(pointer, localX, localY, event)=>{
                     if(this.energy > 0){
-                    this.energy--;
-                    this.energyDisplay.setText(`${this.energy}`);
+                        DEX++;
+                        this.energy--;
+                        this.skillDisplay.setText(`${DEX}`);
+                        this.energyDisplay.setText(`${this.energy}`);
                     }
                 });
                 this.minus.setInteractive().on('pointerdown',(pointer, localX, localY, event)=>{
                     if(this.energy < 3){
+                        DEX--;
                         this.energy++;
+                        this.skillDisplay.setText(`${DEX}`);
                         this.energyDisplay.setText(`${this.energy}`);
                         }
                 });
                 this.exit.setInteractive().on('pointerdown',(pointer, localX, localY, event)=>{
                     if(this.energy == 0){
-                        if(Day % 2 == 1){
-                            Day++;
+                        if(DAY % 2 == 1){
+                            DAY++;
                             this.scene.start("cityScene");
                         }else{
                             this.scene.start("battleScene");
@@ -112,51 +127,67 @@ class City extends Phaser.Scene {
             if(!inEvent){
                 inEvent = true;
                 this.menu = this.add.image(450, 450, 'gymMenu');
-                this.plusEndurance = this.add.image(495, 450, 'plus').setOrigin(0, 0);
-                this.plusStrength = this.add.image(495, 310, 'plus').setOrigin(0, 0);
-                this.minusEndurance = this.add.image(325, 450, 'minus').setOrigin(0, 0);
-                this.minusStrength = this.add.image(325, 310, 'minus').setOrigin(0, 0);
-                //this.intialStr = str;
-                //this.intialEndur = endur;
-
+                this.plusEndurance = this.add.image(495, 310, 'plus').setOrigin(0, 0);
+                this.plusStrength = this.add.image(495, 450, 'plus').setOrigin(0, 0);
+                this.minusEndurance = this.add.image(325, 310, 'minus').setOrigin(0, 0);
+                this.minusStrength = this.add.image(325, 450, 'minus').setOrigin(0, 0);
                 this.exit = this.add.image(450, 750, 'exit');
                 this.energyDisplay = this.add.text(450, 640, `${this.energy}`, { fontFamily: 'Times New Roman', fontSize: '60px', color: '#FFFFFF'}).setOrigin(0.5);
+                this.enduranceDisplay = this.add.text(450, 350, `${END}`, { fontFamily: 'Times New Roman', fontSize: '60px', color: '#FFFFFF'}).setOrigin(0.5);
+                this.strengthDisplay = this.add.text(450, 490, `${STR}`, { fontFamily: 'Times New Roman', fontSize: '60px', color: '#FFFFFF'}).setOrigin(0.5);
+                let firstClick = true;
+                if(firstClick){
+                    firstClick = false;
+                    this.intialStr = STR;
+                    this.intialEndur = END;
+                }
+
                 this.plusEndurance.setInteractive().on('pointerdown',(pointer, localX, localY, event)=>{
                     if(this.energy > 0){
-                    this.energy--;
-                    this.energyDisplay.setText(`${this.energy}`);
+                        END++;
+                        this.energy--;
+                        this.enduranceDisplay.setText(`${END}`);
+                        this.energyDisplay.setText(`${this.energy}`);
                     }
                 });
                 this.minusEndurance.setInteractive().on('pointerdown',(pointer, localX, localY, event)=>{
-                    if(this.energy < 3){//need intial str for bug
+                    if(this.energy < 3 && END > this.intialEndur){
+                        END--;
                         this.energy++;
+                        this.enduranceDisplay.setText(`${END}`);
                         this.energyDisplay.setText(`${this.energy}`);
                         }
                 });
 
                 this.plusStrength.setInteractive().on('pointerdown',(pointer, localX, localY, event)=>{
                     if(this.energy > 0){
-                    this.energy--;
-                    this.energyDisplay.setText(`${this.energy}`);
+                        STR++;
+                        this.energy--;
+                        this.strengthDisplay.setText(`${STR}`);
+                        this.energyDisplay.setText(`${this.energy}`);
                     }
                 });
                 this.minusStrength.setInteractive().on('pointerdown',(pointer, localX, localY, event)=>{
-                    if(this.energy < 3){//need intial str for bug
+                    if(this.energy < 3 && STR > this.intialStr){
+                        STR--;
                         this.energy++;
+                        this.strengthDisplay.setText(`${STR}`);
                         this.energyDisplay.setText(`${this.energy}`);
                         }
                 });
 
                 this.exit.setInteractive().on('pointerdown',(pointer, localX, localY, event)=>{
                     if(this.energy == 0){
-                        if(Day % 2 == 1){
-                            Day++;
+                        if(DAY % 2 == 1){
+                            DAY++;
+                            firstClick = true;
                             this.scene.start("cityScene");
                         }else{
+                            firstClick = true;
                             this.scene.start("battleScene");
                         }
                     }else{
-                    this.clearEvent();
+                    this.clearEventSpecial();
                     inEvent=false;
                     }
                 });
@@ -190,7 +221,12 @@ class City extends Phaser.Scene {
     }
 
     update(){
-        
+        this.endMenu.setText(`${END}`);
+        this.strMenu.setText(`${STR}`);
+        this.witMenu.setText(`${WIT}`);
+        this.dexMenu.setText(`${DEX}`);
+        this.dayMenu.setText(`${15 - DAY}`);
+        this.hpMenu.setText(`${2 * (END - 1) + 14}`);
     }
 
     clearEvent(){
@@ -199,6 +235,7 @@ class City extends Phaser.Scene {
         this.minus.destroy();
         this.energyDisplay.destroy();
         this.exit.destroy();
+        this.skillDisplay.destroy();
     }
 
     clearEventSpecial(){
@@ -208,6 +245,8 @@ class City extends Phaser.Scene {
         this.plusStrength.destroy();
         this.minusStrength.destroy();
         this.energyDisplay.destroy();
+        this.enduranceDisplay.destroy();
+        this.strengthDisplay.destroy();
         this.exit.destroy();
     }
 }
