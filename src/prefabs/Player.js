2e2ef0e6663e2c827903fe24613f73f235c6283d
clairec98 <63259884+clairec98@users.noreply.github.com>
2020-05-20 22:06:12 -0700

    Added monkey asset and json files

--- src/prefabs/Player.js
+++ src/prefabs/Player.js
@@ -10,7 +10,20 @@ class Player extends Phaser.GameObjects.Sprite {
         this.attackText = [
             'Slap', 'With all five fingers, you slap the bear right across the face.\n\n',
             'Scream', "\"REEEEEEEEEEEEE,\" screams the zookeeper. The bear stares at you and blinks confusedly.\n\n",
-            'Cartwheel', "Gracefully, you cartwheel directly into the animal, slamming it with your body.\n\n"
+            'Cartwheel', "Gracefully, you cartwheel directly into the animal, slamming it with your body.\n\n",
+            'Smash', "With the righteous force or your passion for zoos, you smash a rock over the bear's head. Ouch.\n\n",
+            'Intimidate', "You stomp your feet and insult the bear's mother. It now feels bad about itself.\n\n",
+            'Spin Attack', "You sort of do like, this spin, karate thing? An ancient kung fu move. It does some damage.\n\n",
+            'Haymaker', "You wind up your fist behind your back and spring it upon the bear with all your zookeeping might.\n\n",
+            'Persuade', "You have a long chat with the bear and convince it that it to do what you want.\n\n",
+            'Jump Kick', "You jump! And then. . . you kick! You're so nimble, you don't even know what to do with yourself.\n\n",
+            'Toss', "You pick up the bear and toss it aside. You feel pretty good about yourself after.\n\n",
+            'Trap', "You trick the bear into sticking his hand into a pot of honey. He's bamboozled.\n\n",
+            'Maneuver', "You jab and dodge your way around the bear so quickly that it can hardly keep track of you.\n\n",
+            'Rage', "You go full on beast mode on this god dang bear. You go stupid. You go crazy.\n\n",
+            'Trick', "With the ancient art of manipulation, you're able to distract the bear and successfully confuse it.\n\n",
+            'Acrobatics', "You dance around this bear so quickly that you have it's head spinning.\n\n"
+
         ];
     }
 
